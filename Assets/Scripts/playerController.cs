a6219ae8769a4e7f9fd12eab142c39f7c235b554
Goldenlion5648 <coboudinot@gmail.com>
2020-09-19 20:13:58 -0400

    made all clones replay movement

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -10,8 +10,11 @@ public class playerController : MonoBehaviour
     public GameObject robotPrefab;
     public bool isMaster = true;
     float posRecordFrequency = .2f;
-    int currentFrame = 0;
+    public int currentFrame = 0;
     float lastFrameTime = 0;
+
+    bool finishedReplaying = false;
+
     void Start()
     {
         body = transform.GetComponent<Rigidbody>();
@@ -19,11 +22,13 @@ public class playerController : MonoBehaviour
         if (isMaster == false)
             transform.position = positionRecordings[0];
 
+        //Debug.Log("length of recording: " + positionRecordings.Count);
 
     }
 
     void movement()
     {
+        //controls for the robot the player is currently controlling
         var speed = 3.0f;
         body.velocity = new Vector3(0, 0, 0);
 
@@ -47,6 +52,7 @@ public class playerController : MonoBehaviour
 
     void trackPos()
     {
+        //track the position of the player in case a robot has to copy it
         positionRecordings.Add(body.position);
 
     }
@@ -54,23 +60,59 @@ public class playerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
+            //make a clone of the  player that will replay the movements that were just done
             var newCopy = Instantiate(robotPrefab, positionRecordings[0], Quaternion.identity);
-            newCopy.GetComponent<playerController>().isMaster = true;
+            //newCopy.GetComponent<playerController>().isMaster = true;
             this.isMaster = false;
             body.velocity = Vector3.zero;
-            positionRecordings.RemoveAt(positionRecordings.Count - 1);
+
+            //using a for loop because I have been burned in the past from untiy not allowing me to stop a while loop
+            //during run time and crashing the program
+            for (int i = 0; i < 10; i++)
+            {
+                positionRecordings.RemoveAt(positionRecordings.Count - 1);
+                if (positionRecordings[positionRecordings.Count - 1] != positionRecordings[0])
+                    break;
+
+            }
+            Debug.Log("length of recording after: " + positionRecordings.Count);
+
+            //reset all of the clones to their starting positions
+            var robots = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject r in robots)
+            {
+                r.GetComponent<playerController>().currentFrame = 0;
+            }
+
 
         }
     }
 
     void replayMovements()
     {
-        //if (Time.time - lastFrameTime >= posRecordFrequency)
-        //lastFrameTime = Time.time;
+        //playback movements 
         if (currentFrame < positionRecordings.Count)
         {
-            body.position = positionRecordings[currentFrame];
+            body.position = this.positionRecordings[currentFrame];
+            Debug.Log("current frame: " + currentFrame + " " + positionRecordings[currentFrame]);
             currentFrame++;
+
+
+
+        }
+        else
+        {
+            //debug code
+            if (finishedReplaying == false)
+            {
+                finishedReplaying = true;
+                Debug.Log("printing " + positionRecordings.Count + " things");
+
+                foreach (var item in positionRecordings)
+                {
+                    Debug.Log(item);
+                }
+            }
         }
         //Debug.Log("Frame num: " + currentFrame);
         //Debug.Log("Frame length: " + positionRecordings.Count);
@@ -79,6 +121,7 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(transform.name + " is master? " + isMaster);
         if (isMaster)
         {
             movement();
