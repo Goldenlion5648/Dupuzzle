4fa9bd7f18bed64e0e0dc59bd0a51463a4f1db64
Goldenlion5648 <coboudinot@gmail.com>
2020-09-27 23:21:51 -0400

    button now presses and rises

--- Assets/Scripts/buttonScripts.cs
+++ Assets/Scripts/buttonScripts.cs
@@ -5,20 +5,63 @@ using UnityEngine;
 public class buttonScripts : MonoBehaviour
 {
     public GameObject linkedWall;
+    Transform visibleButton;
+    bool isTriggered = false;
+
+    float pressCooldown = 0;
+    float changeAmount = .2f;
+
     // Start is called before the first frame update
     void Start()
     {
+        visibleButton = gameObject.transform.parent.transform.Find("button");
+        InvokeRepeating("cooldown", .1f, changeAmount);
 
     }
 
     private void OnTriggerStay(Collider other)
     {
         //Debug.Log("triggered button");
+        //isTriggered = false;
 
-        if (other.gameObject.layer == 8)
+        if (other.gameObject.layer == 8 && pressCooldown == 0)
         {
+            isTriggered = true;
             if (linkedWall != null)
                 Destroy(linkedWall);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            Debug.Log("left trigger");
+
+            isTriggered = false;
+            pressCooldown = 1;
+        }
+    }
+
+    void cooldown()
+    {
+        pressCooldown = Mathf.Max(pressCooldown - changeAmount, 0);
+
+    }
+
+    void pressAndUnpress()
+    {
+        var current = visibleButton.localScale;
+        if (isTriggered)
+        {
+            visibleButton.localScale = new Vector3(current.x, Mathf.Max(.01f, current.y - .8f * Time.deltaTime), current.z);
+            //visibleButton.position -= new Vector3(0, 1, 0);
+        }
+        else
+        {
+            visibleButton.localScale = new Vector3(current.x, Mathf.Min(.5f, current.y + .2f * Time.deltaTime), current.z);
+
         }
 
     }
@@ -27,6 +70,6 @@ public class buttonScripts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        pressAndUnpress();
     }
 }
